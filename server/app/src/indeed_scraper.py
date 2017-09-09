46278cecdcde3795668e992f9fb2d31985388020
David W Grossman <2gotgrossman@gmail.com>
2017-09-09 10:48:18 -0400

    Renaming database parameters

--- server/app/src/indeed_scraper.py
+++ server/app/src/indeed_scraper.py
@@ -29,7 +29,7 @@ def get_location_from_result(result):
     locs = {}
     possible_zip = int(loc.split()[-1]) if loc.split()[-1].isdigit() else 0
     if possible_zip > 10000 & possible_zip < 100000:
-        locs['zip_code'] = possible_zip
+        locs['zip_code'] = str(possible_zip)
         locs['city_state'] = loc.split()[:-1]
     else:
         locs['zip_code'] = None
@@ -82,23 +82,23 @@ def get_jobs(query, location, radius="15", number_of_pages=5):
     # E.g. Page 1 is start=0, page 2 is start=10, page 3 --> start=20, etc
     for start in range(0,number_of_pages*10, 10):
         r = requests.get(url_template.format(query_for_search, location, radius, start))
-        print url_template.format(query_for_search, location, radius, start)
+        # print url_template.format(query_for_search, location, radius, start)
         soup = BeautifulSoup(r.content, "html5lib")
         results = soup.findAll('div', { "class" : "result" })
 
         for i, result in enumerate(results):
             if result:
                 row = {}
-                row['Query'] = query
-                row['Title'] = get_title_from_result(result)
-                row['Company'] = get_company_from_result(result)
-                row['Summary'] = get_summary_from_result(result)
-                row['Salary'] = get_salary_from_result(result)
-                row['Experience'] = get_experience_from_result(result)
+                row['query'] = query
+                row['title'] = get_title_from_result(result)
+                row['company'] = get_company_from_result(result)
+                row['summary'] = get_summary_from_result(result)
+                row['salary'] = get_salary_from_result(result)
+                row['experience'] = get_experience_from_result(result)
 
                 locs = get_location_from_result(result)
-                row['Zip_Code'] = locs['zip_code']
-                row['City-State'] = locs['city_state']
+                row['zip_code'] = locs['zip_code']
+                row['city_state'] = locs['city_state']
 
                 rows.append(row)
     return rows
@@ -106,7 +106,7 @@ def get_jobs(query, location, radius="15", number_of_pages=5):
 if __name__ == '__main__':
     import time
     t1 = time.time()
-
+    print "Testing indeed_scraper.py"
     get_jobs(query="Software Developer", location="33146")
 
     t2 = time.time()

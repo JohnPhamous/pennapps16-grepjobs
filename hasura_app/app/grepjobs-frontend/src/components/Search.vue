1ab31b9ae438caa9047ebbdc7d1ca6e509f90030
John Pham <johnphammail@gmail.com>
2017-09-09 12:26:02 -0700

    Adding giphy api calls

--- hasura_app/app/grepjobs-frontend/src/components/Search.vue
+++ hasura_app/app/grepjobs-frontend/src/components/Search.vue
@@ -4,28 +4,43 @@
       <div class="row h-100">
         <div class="col-lg-12 my-auto">
           <div class="mx-auto">
-              <div id="gif-container" v-if="!isSearchShown">
-                <center>
-                  <h1 id="search-title">Searching...</h1>
-                  <img id="gif-title" class="gif" src="https://media2.giphy.com/media/3ohze15TQsHnWxQfm0/giphy.gif"/>
-                  <img id="gif-location" class="gif" src="https://media0.giphy.com/media/pTeT1LFAWfQEo/giphy.gif"/>
-                </center>
-              </div>
-
-              <div id="searchField" v-show="isSearchShown">
-                <h1 class="mb-5 search animated fadeInDown">
-                    I am looking for a
-                    <input id="job-title" class="search-input" :style="{width: this.jobTitle.length * this.paddingTolerance + 'px'}" type="text" v-model='jobTitle' spellcheck="false"/>
-                    job
-                </h1>
-                <h1 class="animated fadeInDown">
-                    in <input id="job-location" class="search-input" type="text" :style="{width: this.jobLocation.length * this.paddingTolerance + 'px'}" v-model='jobLocation' spellcheck="false"/>
-                </h1>
-                <br />
-                <div class="text-center animated fadeInDown">
-                    <div class="btn btn-outline btn-xl animated pulse infinite" v-on:click="findJob">Find your dream job</div>
+            <div id="gif-container" v-if="!isSearchShown">
+              <center>
+                <div class="row">
+                  <div class="col-md-12">
+                    <h1 id="search-title"><strong>Looking for...</strong></h1>
+                  </div>
+                  <div class="col-md-5">
+                    <img id="gif-title" class="gif" v-bind:src="titleGif"/>
+                    <h2>{{ this.jobTitle }}</h2>
+                  </div>
+                  <div class="col-md-2">
+                    <h2 id="search-load">Jobs In</h2>
+                  </div>
+                  <div class="col-md-5">
+                    <img id="gif-location" class="gif" v-bind:src="locationGif"/>
+                    <h2>{{ this.jobLocation }}</h2>
+                  </div>
                 </div>
+              </center>
+            </div>
+
+            <div id="searchField" v-show="isSearchShown">
+              <h1 class="mb-5 search animated fadeInDown">
+                  I am looking for a
+                  <input id="job-title" class="search-input" :style="{width: this.jobTitle.length * this.paddingTolerance + 'px'}" type="text" v-model='jobTitle' spellcheck="false"/>
+                  job
+              </h1>
+              <h1 class="animated fadeInDown">
+                  in <input id="job-location" class="search-input" type="text" :style="{width: this.jobLocation.length * this.paddingTolerance + 'px'}" v-model='jobLocation' spellcheck="false"/>
+              </h1>
+
+              <br />
+
+              <div class="text-center animated fadeInDown">
+                  <div class="btn btn-outline btn-xl animated pulse infinite" v-on:click="findJob">Find your dream job</div>
               </div>
+            </div>
           </div>
         </div>
       </div>
@@ -39,7 +54,9 @@ export default {
   data () {
     return {
       jobTitle: '',
+      titleGif: '',
       jobLocation: '',
+      locationGif: '',
       paddingTolerance: 30,
       isSearchShown: true
     }
@@ -47,6 +64,19 @@ export default {
   methods: {
     findJob: function () {
       this.isSearchShown = false
+      this.$http.get(`http://api.giphy.com/v1/gifs/search?api_key=c6caa15f718f4a64859883c625d3d5ea&q=${this.jobTitle}&limit=1`).then(response => {
+        this.titleGif = response['body']['data']['0']['images']['original']['url']
+      },
+      response => {
+        console.log('Error')
+      })
+
+      this.$http.get(`http://api.giphy.com/v1/gifs/search?api_key=c6caa15f718f4a64859883c625d3d5ea&q=${this.jobLocation + ' city'}&limit=1`).then(response => {
+        this.locationGif = response['body']['data']['0']['images']['original']['url']
+      },
+      response => {
+        console.log('Error')
+      })
     }
   }
 }

47b9efe3b47f8084ca06fbd54740a9ff85a7cb3d
David W Grossman <2gotgrossman@gmail.com>
2017-09-10 02:14:25 -0400

    added caching to add_latlong.py

--- hasura_app/app/src/add_latlong.py
+++ hasura_app/app/src/add_latlong.py
@@ -5,7 +5,6 @@ import json
 CURRENT = 0
 
 
-
 data_dir = "/Users/David/code/pennapps16-grepjobs/hasura_app/app/src/data/"
 data_dir2 = "/Users/David/code/pennapps16-grepjobs/hasura_app/app/src/data2/"
 city = os.listdir(data_dir)[CURRENT]
@@ -21,13 +20,15 @@ for entry in ARR:
     try:
         company_tag = entry['company'] + " " + entry['city_state']
         print company_tag
-        lat_long = scraper.get_google_thingy_lat_long(company_tag)
-
-        dict_of_company_locations[company_tag] = lat_long
+        if company_tag in dict_of_company_locations:
+            lat_long = dict_of_company_locations[company_tag]
+        else:
+            lat_long = scraper.get_google_thingy_lat_long(company_tag)
+            dict_of_company_locations[company_tag] = lat_long
 
         entry.update(lat_long)
-
         NEW_DATA.append(entry)
+        
     except ValueError:
         print("shit went wrong for: " + company_tag)
     finally:
